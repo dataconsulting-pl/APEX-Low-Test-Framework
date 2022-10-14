b3984fa7e2da509d02d34e667b48085b0fbf4f32
Krzysztof Borowski <kborowski@dataconsulting.pl>
2022-10-14 13:09:51 +0200

    Added new browser options - acceptInsecureConnection

--- src/main/java/pl/dataconsulting/APEX_TAF/framework/config/WebDriverConfig.java
+++ src/main/java/pl/dataconsulting/APEX_TAF/framework/config/WebDriverConfig.java
@@ -3,7 +3,9 @@ package pl.dataconsulting.APEX_TAF.framework.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -27,13 +29,18 @@ public class WebDriverConfig {
     @Value("${wait.implicit:10000}")
     private Duration implicitWait;
 
+    @Value("${driver.acceptInsecureConnection:false}")
+    private boolean acceptInsecureConnection;
+
 
     @BeanThreadScope
     @Scope("driverscope")
     @ConditionalOnProperty(name = "browser", havingValue = "chrome")
     public WebDriver chromeDriver() {
         WebDriverManager.chromedriver().setup();
-        ChromeDriver chromeDriver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setAcceptInsecureCerts(acceptInsecureConnection);
+        ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
         chromeDriver.manage().timeouts().implicitlyWait(implicitWait);
         return chromeDriver;
     }
@@ -42,7 +49,9 @@ public class WebDriverConfig {
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver firefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setAcceptInsecureCerts(acceptInsecureConnection);
+        FirefoxDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
         firefoxDriver.manage().timeouts().implicitlyWait(implicitWait);
         return firefoxDriver;
     }
